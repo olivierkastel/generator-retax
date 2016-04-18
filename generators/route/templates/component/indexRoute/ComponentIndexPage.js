c3576896c9a6776dc529a85ba9b5671b2a309f3c
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-04-18 14:43:37 -0700

    pureRender is now imported from 'pure-render-decorator'

--- generators/route/templates/component/indexRoute/ComponentIndexPage.js
+++ generators/route/templates/component/indexRoute/ComponentIndexPage.js
@@ -1,7 +1,7 @@
 import React, { PropTypes, Component } from 'react';
 <% if (pureRender) { -%>
     
-import { pureRender } from 'decorators';
+import pureRender from 'pure-render-decorator';
 <% } -%>
 
 <% if (pureRender) { -%>
